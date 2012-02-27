1d6a9a158f25791254902e1a8fc0187db3c42dfb
Nicholas Watson <nwwatson@gmail.com>
2012-02-27 08:04:41 -0500

    added session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,14 @@
 Alohomora::Engine.routes.draw do
+  
+  get "session/new"
+
+  get "session/create"
+
+  post "account/create"
+  
+  scope constraints: lambda { |r| r.env['warden'].user.nil? } do
+    get "signup", to: "users#new", as: "signup"
+    get "login", to: "sessions#new", as: "login"
+  end
+
 end
