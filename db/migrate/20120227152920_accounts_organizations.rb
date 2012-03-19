a0e13db83762e3a6e07698946b1fa7331e4bc8a1
Nicholas Watson <nwwatson@gmail.com>
2012-03-19 09:57:48 -0400

    Removed unneeded files

--- db/migrate/20120227152920_accounts_organizations.rb
+++
@@ -1,8 +0,0 @@
-class UsersOrganizations < ActiveRecord::Migration
-  def change
-    create_table :users_organizations do |t|
-      t.integer :user_id
-      t.integer :organization_id
-    end
-  end
-end
