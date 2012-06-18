07c541b1b0d53481e89e533c9998d4555b07d661
Nicholas Watson <nwwatson@gmail.com>
2012-06-18 16:25:10 -0400

    removed test directory in favor of rspec

--- test/alohomora_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AlohomoraTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Alohomora
-  end
-end
--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '54fa21910ce4a5dc40a4368974c0862e65a6fc28b2a8f94bc0da2e7c934e96d51f006ce435b6428449bd6943be6e694763f3fa005a3601a30033ed66265f543f'
