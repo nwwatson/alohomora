f9fff8cb3060ce13a2d84ced8327fe7e893f4875
Nicholas Watson <nwwatson@gmail.com>
2012-07-10 00:05:50 -0400

    updated organizations api to contain restful interface

--- app/controllers/alohomora/api/v1/organizations_controller.rb
+++ app/controllers/alohomora/api/v1/organizations_controller.rb
@@ -7,9 +7,41 @@ class Alohomora::Api::V1::OrganizationsController < ApplicationController
   end
   
   def show
-    @organization = Alohomora::Organization.get_user_organziation(params[:id], current_user)
+    @organization = Alohomora::Organization.get_organization_organziation(params[:id], current_organization)
   end
   
+  
+  def create
+    @organization = Alohomora::Organization.new(params[:organization])
+    
+    if @organization.save
+      respond_with(@organization, :responder => Alohomora::Responder)
+    else
+      render :json => { :success => false, :errors => @organization.errors }, :status => :unprocessable_entity
+    end
+
+  end
+  
+  
+  def update
+    @organization = Alohomora::Organization.find(params[:id])
+
+    if @organization.update_attributes(params[:organization])
+      respond_with(@organization, :responder => Alohomora::Responder)
+    else
+      render :json => { :success => false, :errors => @organization.errors }, :status => :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    begin
+      @organization = Alohomora::Organization.get_accessible(params[:organization_id], current_organization).find(params[:id])
+      @organization.destroy
+      return render :json => {:success => true }, :status => 200
+    rescue
+      return render :json => {:success => false, :errors => ["Unable to delete organization"]}, :status => 401
+    end
+  end
   
   
 end
